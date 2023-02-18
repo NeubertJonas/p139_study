642ed8db636024e726a684616af23c1069664af7
Jonas Neubert <dev@jonaz.de>
2023-02-18 15:34:03 +0100

    First working version for audio-video-sync

--- transcribing/test/app.py
+++ transcribing/test/app.py
@@ -1,22 +1,20 @@
 """Python module to sync audio and video files."""
+import os
 from os import path
 import subprocess
 import requests
 
 
-def run_cmd(*args):
-    p = subprocess.Popen(
-        *args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT
-    )
-    out, error = p.communicate()
-    return out, error
+def main():
+    """""Main function"""
+    if check_praat() and check_ffmpeg():
+        print("All requirements met")
+    else:
+        print("Requirements not met")
+        print("Please install the required software and try again")
+        exit()
 
-
-ps_command = "& {{Start-Process cmd.exe -argumentlist 'ipconfig' -Verb Runas}}"
-command = ['powershell.exe', '-command', ps_command]
-run_cmd(command)
+    sync()
 
 
 def check_praat():
@@ -25,7 +23,7 @@ def check_praat():
         print("Praat.exe found")
         return True
     else:
-        print("Praat.exe not found")
+        print("ERROR: Praat.exe not found")
         print("Download the latest version from https://github.com/praat/praat/releases")
         print("Extract the zip file and place Praat.exe in the same folder as this script")
         return False
@@ -35,51 +33,86 @@ def check_praat():
 def check_ffmpeg():
     """""Check if ffmpeg is installed"""
     try:
-        subprocess.run("ffm2peg", check=False, stdout=subprocess.DEVNULL)
+        subprocess.run("ffmpeg", check=False,
+                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         print("FFmpeg installed")
         return True
     except OSError:
-        print("FFmpeg not installed")
-        print("You can either run 'install_ffmpeg.ps1' as admin to automatically install it via the Chocolatey package manager or follow the instructions below.")
-        print("https://www.wikihow.com/Install-FFmpeg-on-Windows")
-        print("(The essentials version is sufficient for this script)")
+        print("ERROR: FFmpeg not installed")
+        print("Would you like to receive instructions on how to install? (on Windows)")
+        instructions = input(
+            "Press 'y' to continue or any other key to exit: ")
+        if instructions == "y":
+            print(
+                "\nThe easiest way is to utilize Chocolatey, a package manager for Windows.")
+            print("1. Open an elavated PowerShell prompt (run as administrator)")
+            print("2. Run the following command to install Chocolatey:")
+            print(
+                "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
+            print("For more info, see here: https://chocolatey.org/install")
+            print("3. Close PowerShell and open a new elavated PowerShell prompt.")
+            print("4. Run the following command:")
+            print("choco install ffmpeg\n")
         return False
 
 
-def main():
-    """""Main function"""
-    if check_praat() and check_ffmpeg():
-        print("All requirements met")
+def sync():
+    """""Sync audio and video files"""
+
+    print("Have you copied the audio and video file to the media folder?")
+    print("Please enter the name of the video file (including extension)")
+    video = "media/" + input("Video file: ")
+    print("Please enter the name of the audio file (including extension)")
+    audio = "media/" + input("Audio file: ")
+
+    # Compare audio streams
+    subprocess.run(
+        f"ffmpeg -i {video} -ar 48k media/temp_audio.wav", shell=True, check=True)
+    offset = compare(audio)
+    os.remove("media/temp_audio.wav")
+
+    if offset[0] == "-":
+        offset = offset[1:]
+        subprocess.run(
+            f"ffmpeg -itsoffset 00:00:{offset[0:5]} -i {video} -i {audio} -crf 18 -map 0:v -map 1 media/output.mp4", shell=True, check=True)
+        # Audio starts first and video second (i.e., video is delayed)
     else:
-        print("Requirements not met")
-        print("Please install the required software")
-        exit()
-    extract()
-    sync()
-    output()
+        offset = float(offset[0:7])
+        offset = int(offset * 1000)
+        print(offset)
+        subprocess.run(
+            f"ffmpeg -i {video} -i {audio} -af adelay=\"{offset}|{offset}\" -crf 18 -map 0:v -map 1 media/output.mp4", shell=True, check=True)
+        # Video starts first and audio second (i.e., audio is delayed)
 
 
-runCmd(ipconfig)
+def compare(audio):
+    """Run Praat script to determine offset"""
 
-# Important: Make sure the sampling rate is the same for both files
-# If not, use ffmpeg to convert the audio file to the same sampling rate as the video file
-# ffmpeg -i input.wav -ar 48k output.wav
+    # Important: Make sure the sampling rate is the same for both files
+    # If not, use ffmpeg to convert the audio file to the same sampling rate as the video file
+    # Webcam (EEG lab): 48kHz
+    # iPad: 44.1kHz
+    # ffmpeg -i input.wav -ar 48k output.wav
+    # TODO: Compare sampling rates of audio files and convert if necessary
+    # Not sure how to determine sampling rate without relying on more packages
+    # So instead I'll just convert to 48khz every time and hope for the best (see line 70)
+    # Converting every time when extracting from video
+    # Because Roland is always recording at 48k
+
+    result = subprocess.run(f"Praat.exe --run crosscorrelate.praat {audio} media/temp_audio.wav 0 30",
+                            capture_output=True, check=True, encoding="utf-8")
+
+    result = result.stdout
+    result = result.replace("\x00", "")
+    return (result)
 
 
-# Instead:
-# TODO: Figure out best setting for audio/video conversion via ffmpeg
+# TODO: Figure out best setting for audio/video conversion via ffmpeg. (uUse -crf 18 for now)
+
+# Old code:
+""" 
 subprocess.run(
     "ffmpeg -itsoffset 00:00:03.06 -i video.mp4 -i audio.wav -crf 18 -map 0:v -map 1 output.mp4")
-
-
-def extract():
-    ffmpeg = (
-        FFmpeg()
-        .option("y")
-        .input("video.mp4")
-        .output("output.wav", ar="48k")
-    )
-    ffmpeg.execute()
 
 
 def sync():
@@ -158,10 +191,9 @@ result = subprocess.run(['Praat.exe', '--run', 'crosscorrelate.praat',
 print(result)
 
 
-""" def export():
-    xx """
+
+    """
+# Old code end.
 
 if __name__ == "__main__":
-    sync()
-
-    replace()
+    main()
