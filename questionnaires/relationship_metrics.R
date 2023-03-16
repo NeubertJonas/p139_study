5db5625d611a7c07c15c2c666862e67c740a223a
Jonas Neubert <dev@jonaz.de>
2023-03-17 00:22:10 +0100

    new functions and consistent assignment operator

--- questionnaires/relationship_metrics.R
+++ questionnaires/relationship_metrics.R
@@ -6,20 +6,19 @@ library(tidyverse)
 
 # Import data, filter non-participant responses, and
 # remove unnecessary columns
-drop_col = c("StartDate", "EndDate", "Status", "IPAddress", "Progress",
+drop_col <- c("StartDate", "EndDate", "Status", "IPAddress", "Progress",
            "ResponseID","RecipientLastName", "RecipientFirstName",
            "RecipientEmail", "ExternalReference", "LocationLatitude",
            "LocationLongitude", "DistributionChannel", "UserLanguage")
 
 
-dat = read_csv("data/questionnaire_2.csv", show_col_types = FALSE) |> 
+dat <- read_csv("data/questionnaire_2.csv", show_col_types = FALSE) |> 
   filter(grepl("139", Q2)) |> 
   select(-any_of(drop_col)) |> 
-  mutate(across(Q3:Q49 | Q51_1 | Q52_1, as.integer))
+  mutate(across(Q3:Q49 | Q51_1 | Q52_1, as.integer)) |> 
+  mutate(across(Q2, as.character))
+
 rm(drop_col)
-
-# Calculate questionnaire results
-
 
 
 # Overview of all questionnaires
@@ -36,42 +35,72 @@ scales <- tribble(
   "IIEF", "Q35", 15
 )
 
+
+# Generic Helper Functions ------------------------------------------------
+
 # Column name of first question for a scale
-first_question = \(x) {
+first_question <- \(x) {
   return(
     scales |> filter(name == x) |> pull(start)
     )
 }
 
 # Item count for a scale
-get_count = \(x) {
+get_count <- \(x) {
   return(
     scales |> filter(name == x) |> pull(item_count)
     )
 }
 
 # Range of columns belonging to a scale
-get_range = \(dat, name) {
-  x = which(names(dat) == first_question(name))
-  y = x + get_count(name) - 1
-  z = c(x:y)
+get_range <- \(dat, name) {
+  x <- which(names(dat) == first_question(name))
+  y <- x + get_count(name) - 1
+  z <- c(x:y)
   return(z)
 }
 
+# Results for Scales
+# Leaving "participant" and "column" blank will return data for
+# all participants and all scales
+# Vectors are allowed as input, e.g.,
+# get_results(dat, c("13902", "P13901"), c("CSI_4", "SWLS"))
 
+get_results <- \(dat, participant = NA, column = NA) {
+  
+  if (is.logical(participant) & is.logical(column)) {
+    results <- dat |> select(any_of(c("Q2", scales$name)))
+    
+  } else if (!is.logical(participant) & !is.logical(column)) {
+    results <- dat |> select(any_of(c("Q2", scale))) |> 
+      filter(Q2 == participant)
+    
+  } else if (!is.logical(scale)) {
+    results <- dat |> select(any_of(c("Q2", column)))
+    
+  } else if (!is.logical(participant)) {
+    results <- dat |> select(any_of(c("Q2", scales$name))) |> 
+      filter(Q2 == participant)
+  } 
+  
+  return(results)
+}
+
+# Questionnaire Functions -------------------------------------------------
+# Note. Scores for scales will be NA if any questions were skipped.
 
 # Couple Satisfaction Index (CSI-4)
-# Q5 - Q8
 # Qualtrics records the lowest response as "1", but for scoring
 # it is changed to "0".
 
-add_csi = \(dat) {
-  range = get_range(dat, "CSI_4")
-  csi = dat |> select(all_of(range))
-  dat = dat |> mutate(CSI_4 = rowSums(csi) - 4, .after = max(range))
+add_csi <- \(dat) {
+  range <- get_range(dat, "CSI_4")
+  csi <- dat |> select(all_of(range))
+  dat <- dat |> mutate(CSI_4 = rowSums(csi) - 4, .after = max(range))
   return(dat)
 }
-  # Note. Scores for scales will be NA if any questions were skipped.
+
+dat = add_csi(dat)
   
 
 get_csi = \(dat) {
@@ -81,11 +110,6 @@ get_csi = \(dat) {
 }
 
 
-get_scale = \(dat, participant, scale) {
-  return(
-    dat |> select(all_of(c(scale, "Q2"))) |> filter(Q2 == participant)
-  )
-}
 
 add_swls = \(dat) {
   swls = dat |> mutate(SWLS = swls_cols , .after = Q9_5)
