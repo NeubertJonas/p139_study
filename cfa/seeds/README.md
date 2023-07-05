e52e4f290776137f59c0da05e8c6a673add9af2e
Jonas Neubert <dev@jonaz.de>
2023-07-05 11:44:46 +0200

    added documentation

---
+++ cfa/seeds/README.md
@@ -0,0 +1,47 @@
+# Seed Words
+
+Twelve seed words were selected for the CFA; Four each with emotional, abstract, and neutral connotation.
+Selection was based on [LexOPS by Jack Taylor](https://jackedtaylor.github.io/LexOPSdocs/), which contains an English-language database of words and their attributes based on different studies.
+
+## Variables
+
+The following variables were used:
+
+| Variable | Description | Source |
+| -------- | ----------- | ------ |
+| PoS.SUBTLEX_UK | Dominant parts of speech according to SUBTLEX-UK | https://doi.org/10.1080/17470218.2013.850521 |
+| Zipf.SUBTLEX_UK | Zipf frequencies `(log10(frequency_per_million)+3)` calculated from UK subtitles | https://doi.org/10.1080/17470218.2013.850521 |
+| AROU.Glasgow_Norms | Arousal ratings (1-9; less-more) from the Glasgow Norms | http://doi.org/10.3758/s13428-018-1099-3 |
+| CNC.Glasgow_Norms | Concreteness ratings (1-7; low-high) from the Glasgow Norms | http://doi.org/10.3758/s13428-018-1099-3 |
+| VAL.Glasgow_Norms | Valence ratings (1-9; more negative-more positive) from the Glasgow Norms | http://doi.org/10.3758/s13428-018-1099-3 |
+
+For the other variables available in LexOPS, [see here](https://rdrr.io/github/JackEdTaylor/LexOPS/man/lexops.html).
+
+SUBTLEX_UK, a database based on British TV/movie subtitle data, was chosen because it features more colloquially used words than other (for example, literature-based) datasets.
+
+More information about the Glasgow Norms can be found in the supplementary material, such as the [Instructions for Rating Tasks](https://static-content.springer.com/esm/art%3A10.3758%2Fs13428-018-1099-3/MediaObjects/13428_2018_1099_MOESM1_ESM.pdf) and the complete [Glasgow Norms dataset](https://static-content.springer.com/esm/art%3A10.3758%2Fs13428-018-1099-3/MediaObjects/13428_2018_1099_MOESM2_ESM.csv).
+
+### Limitation
+
+In comparison to the full Glasgow Norms dataset (5.500 entries), LexOPS does not differentiate when a word has multiple meanings.
+For example, the word "bar" features five entries in the Glasgow Norms: "bar", "bar (legal)", "bar (metal)", "bar (musical)", "bar (pub)" with distinct ratings.
+LexOPS contains a simplified selection of the Glasgow Norms by only including "bar" (i.e., the ratings participants provided for this word when the meaning was not specified).
+
+## Selection Procedure
+
+The LexOPS dataset (containing 262.532 entries) was first narrowed down by only selecting nouns (`PoS.SUBTLEX_UK == "noun"`) and words from the Glasgow Norms dataset, resulting in a smaller dataset of 2.945 words.
+Afterwards, the ratings for arousal, valence, and concreteness were used to identify three subsets based on the following criteria:
+
+| Subset | Criteria | Number of Words |
+| Emotional | High in valence and arousal; Concreteness above average (to differentiate from abstract words) | 284 |
+| Abstract | Average valence and arousal; Low in concreteness (i.e., high in abstractness) | 60 |
+| Neutral | Average valence and arousal; Concreteness above average | 45 |
+
+Finally, all items were sorted by word frequency (Zipf.SUBTLEX_UK) and two researchers selected suitable words from the high-ranking results.
+
+## Results
+
+| Subset | Seeds |
+| Emotional | world, people, mother, father |
+| Abstract | mood, illusion, pride, time |
+| Neutral | taxi, jacket, boot, tower |
--- cfa/seeds/seed_words.r
+++ cfa/seeds/seed_words.r
@@ -1,72 +1,103 @@
-# Generate seed words for the Word Association Task
+# Generate Seed Words -----------------------------------------------------
 
-# Install prerequisites
+# Study: P139
+# Task: Chain Free Association (CFA)
+#
+# Determines twelve seed words:
+# Four each with emotional, abstract, and neutral connotation.
+#
+# Link to GitHub repository:
+# https://github.com/NeubertJonas/p139_study/tree/main/cfa/seeds
+#
+# Author: Jonas Neubert (https://neubert.eu)
+# last updated: 05.07.2023
+
+# Load Packages -----------------------------------------------------------
+
 if (!require("devtools")) install.packages("devtools")
 if (!require("LexOPS")) devtools::install_github("JackEdTaylor/LexOPS@*release")
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 library(LexOPS)
 library(dplyr)
 library(tidyselect)
 
+# Create Dataset ---------------------------------------------------------
 
-# Filtering and sorting the lexops dataset
-# - Removing unnecessary columns
-# - Sorting by word frequency (Zipf.SUBTLEX_UK)
-# - Only keeping nouns (PoS.SUBTLEX_UK)
-# Other options for word frequency and word type are available
-# SUBTLEX_UK is based on British TV/movie subtitle data
-
-
-dataset <- lexops %>%
-  filter(!is.na(VAL.Glasgow_Norms) &
-           PoS.SUBTLEX_UK == "noun" | PoS.SUBTLEX_UK == "unclassified") %>%
-  select(contains(c("string", "Zipf.SUBTLEX_UK",
-                    "length", "Glasgow_Norms"))) %>%
+dataset <- lexops |>
+  filter(
+    !is.na(VAL.Glasgow_Norms) &
+      PoS.SUBTLEX_UK == "noun"
+  ) |>
+  select(contains(c(
+    "string", "Zipf.SUBTLEX_UK",
+    "length", "Glasgow_Norms"
+  ))) |>
   arrange(desc(Zipf.SUBTLEX_UK))
 
-# The Glasgow Norms are used to classify words.
-# See here: https://link.springer.com/article/10.3758/s13428-018-1099-3
-# Note: words with multiple meanings are not included in lexops
+# Description
+# 1. Import LexOPS dataset
+# 2. Filter: Only words with Glasgow Norms and only nouns
+# 3. Select only required columns
+# 4. Sort by word frequency (Zipf.SUBTLEX_UK)
 
-# In case you wanna have a look at the distritbution in the dataset
- summary(dataset$VAL.Glasgow_Norms)
+# Emotional ---------------------------------------------------------------
 
-# EMOTIONAL
 # High in valence and arousal
 # Concreteness above average (to differentiate from abstract words)
+
 emotional <- dataset %>%
   filter(VAL.Glasgow_Norms > 6 &
-  #         AROU.Glasgow_Norms > 4.6 &
-           CNC.Glasgow_Norms > 5)
-cat(nrow(emotional), "emotional words.")
-# Selection: world, people, mother, father
+    AROU.Glasgow_Norms > 4.6 &
+    CNC.Glasgow_Norms > 5)
+
+cat(nrow(emotional), "emotional words identified.")
+
+# Final, human selection: world, people, mother, father
 
 
-# ABSTRACT
+# Abstract ----------------------------------------------------------------
+
 # Average valence and arousal
 # Low in concreteness (i.e., high in abstractness)
 # AROU to 3.9 to include "time"
+
 abstract <- dataset %>%
   filter(between(VAL.Glasgow_Norms, 3.9, 6) &
-           between(AROU.Glasgow_Norms, 3.9, 6) &
-           CNC.Glasgow_Norms < 3)
-cat(nrow(abstract), "abstract words.")
-# Selection: mood, illusion, pride, time
+    between(AROU.Glasgow_Norms, 3.9, 6) &
+    CNC.Glasgow_Norms < 3)
+
+cat(nrow(abstract), "abstract words identified.")
+
+# Final, human selection: mood, illusion, pride, time
 
 
-# NEUTRAL
+# Neutral -----------------------------------------------------------------
+
 # Average valence and arousal
 # Concreteness above average
 
 neutral <- dataset %>%
   filter(between(VAL.Glasgow_Norms, 4.85, 5.45) &
-           between(AROU.Glasgow_Norms, 4.3, 4.9) &
-           CNC.Glasgow_Norms > 6)
-cat(nrow(neutral), "neutral words.")
-# Selection: taxi, jacket, boot, tower
+    between(AROU.Glasgow_Norms, 4.3, 4.9) &
+    CNC.Glasgow_Norms > 6)
 
-# Output to csv (optional)
-# write.csv(emotional[, 1:2], "output/emotional.csv")
-# write.csv(abstract[, 1:2], "output/abstract.csv")
-# write.csv(neutral[, 1:2], "output/neutral.csv")
+cat(nrow(neutral), "neutral words identified.")
+
+# Final, human selection: taxi, jacket, boot, tower
+
+# Raw Output (optional) ---------------------------------------------------
+
+# Set working directory to current file location
+# Either in RStudio
+# (Session > Set Working Directory > To Source File Location)
+# or by uncommenting the API shortcut below.
+
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# write.csv(emotional[, 1:2], "_output/emotional.csv")
+# write.csv(abstract[, 1:2], "_output/abstract.csv")
+# write.csv(neutral[, 1:2], "_output/neutral.csv")
+
+# Clean-Up ----------------------------------------------------------------
+
+# rm(dataset, emotional, abstract, neutral)
